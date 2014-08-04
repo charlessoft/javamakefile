3bdbe49876fdf860d60e61742ca087b68f14fdeb
root <root@ubuntu.(none)>
2014-08-04 16:40:59 +0800

    add makefile for log4j

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ SOURCES=./Say.java \
 JAVAC=javac
 JAVA=java
 PACKAGE=com.csdn.test
-
+CLASSPATH=.:"/home/charles/javamakefile/lib/log4j-1.2.15.jar":"/home/charles/javamakefile/com/csdn/test"
 target:
-	$(JAVAC) -d . $(SOURCES)
+	$(JAVAC) -classpath $(CLASSPATH) -d . $(SOURCES)
 run:
-	$(JAVA) $(PACKAGE).Say
+	$(JAVA) -classpath $(CLASSPATH) $(PACKAGE).Say
 
--- Say.java
+++ Say.java
@@ -1,14 +1,30 @@
 package com.csdn.test;
 
+import java.util.Date;
+import org.apache.log4j.BasicConfigurator;  
+import org.apache.log4j.Logger; 
+
 public class Say {
 
     /**
      *      * @param args
      *           */
-    public static void main(String[] args) {
-        Person p = new Person();
-        String strName = p.GetName();
-        System.out.println(strName);
-    }
+
+    private static final Logger logger = Logger.getLogger(Say.class);  
+    public static void main(String[] args) {  
+        System.out.println("hello world");
+        BasicConfigurator.configure();  
+        logger.info("Hello World, This is an information message.");  
+        logger.error("Hello World, This is a error message.");  
+        logger.warn("Hello World, This is a warning message.");  
+        logger.debug("Hello World, This is a debugging message.");  
+        logger.fatal("Hello World, This is a fatal message.");  
+        System.exit(0);  
+    }  
+    //public static void main(String[] args) {
+    //    Person p = new Person();
+    //    String strName = p.GetName();
+    //    System.out.println(strName);
+    //}
 
 }
